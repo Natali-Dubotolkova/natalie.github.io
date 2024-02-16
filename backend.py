715a4dab195d51c854ba2f5eb54f23cbae1008d3
Natalie <nddubotolkova@basistech.ru>
2024-02-16 16:46:43 +0300

    Add advanced task

--- backend.py
+++ backend.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, send_from_directory, url_for
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return  send_from_directory('static', filename)
+
 @app.route('/profile')
 def profile():
-    return app.send_static_file("/static/profile.html")
+    return  render_template('profile.html')
+
+@app.route('/')
+def redirect_to_profile():
+    return  redirect(url_for('profile'))
+
+
+app.route('/profile_render')
+def profile_render():
+    name="Natalie Dubotolkova"
+    return render_template('profile_render.html',name=name)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=5000, debug=True)
